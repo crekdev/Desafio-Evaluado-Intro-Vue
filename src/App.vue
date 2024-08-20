3685c44680d8cfaace9bbcc818379db57ea4ee29
crekdev <cristobal.sanrodriguez@gmail.com>
2024-08-20 18:59:30 -0400

    Entrega desafio-intro-Vue

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      run: "22.222.222-2",
+      apellidos: "MUSK",
+      nombres: "ELON REEVE",
+      nacionalidad: "SUDAFRICANO",
+      sexo: "M",
+      fechaDeNacimiento: "28 JUN 1971",
+      numeroDeDocumento: "222.222.222",
+      fechaDeEmision: "01 ENE 2025",
+      fechaDeVencimiento: "01 ENE 2030",
+      firmaDeTitular: "Gr4n-C.TM",
+
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="contenedor">
+    <div class="carnet">
+      <aside>
+        <div>
+          <span>CEDULA DE INDENTIDAD</span>
+          <img width="40" src="./chile_bandera.png" alt="" />
+        </div>
+
+        <img src="./foto_elon.jpg" alt="" />
+        <p>RUN {{ run }}</p>
+      </aside>
+
+      <main>
+        <header>
+          <p>REPUBLICA DE CHILE</p>
+          <span>SERVICIO DE REGISTRO CIVIL E IDENTIFICACIÓN</span>
+        </header>
+        <section>
+          <article>
+            <header>APELLIDOS</header>
+            <span>{{ apellidos }}</span>
+          </article>
+          <article>
+            <header>NOMBRES</header>
+            <span>{{ nombres }}</span>
+          </article>
+
+          <div class="grid-2-colums">
+            <article>
+              <header>NACIONALIDAD</header>
+              <span>{{ nacionalidad }}</span>
+            </article>
+            <article>
+              <header>SEXO</header>
+              <span>{{ sexo }}</span>
+            </article>
+          </div>
+          <div class="grid-2-colums">
+            <article>
+              <header>FECHA DE NACIMIENTO</header>
+              <span>{{ fechaDeNacimiento }} </span>
+            </article>
+            <article>
+              <header>NÙMERO DOCUMENTO</header>
+              <span>{{ numeroDeDocumento }}</span>
+            </article>
+          </div>
+          <div class="grid-2-colums">
+            <article>
+              <header>FECHA DE EMISIÒN</header>
+              <span>{{ fechaDeEmision }}</span>
+            </article>
+            <article>
+              <header>FECHA DE VENCIMIENTO</header>
+              <span>{{ fechaDeVencimiento }}</span>
+            </article>
+          </div>
+          <article>
+            <header>FIRMA DEL TITULAR</header>
+            <span>{{ firmaDeTitular }}</span>
+          </article>
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#contenedor {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  font-size: 10px;
+  font-family: sans-serif;
+}
+
+.carnet {
+  width: 400px;
+  height: 230px;
+  background: #c8deed;
+  border-radius: 20px;
+  display: grid;
+  grid-template-columns: 30% 1fr;
+  padding: 15px;
+  box-shadow: 0px 0px 10px 2px black;
+}
+
+aside div {
+  display: flex;
+}
+
+aside>img {
+  margin-top: 15px;
+  width: 100%;
+}
+
+aside p {
+  text-align: center;
+  font-size: 12px;
+  font-weight: bold;
+}
+</style>
